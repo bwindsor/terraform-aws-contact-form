36993cf644739dfa4392e351d6ecd7769d5cdfcb
Ben Windsor <ben.windsor@skyrad.co.uk>
2022-10-17 19:36:15 +0100

    Expose API endpoint as output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "contact_form_post_url" {
   description = "URL to POST contact form requests to"
   value = "${aws_apigatewayv2_stage.gateway_stage.invoke_url}/contactform"
 }
+
+output "api_endpoint" {
+  description = "Base URL for the created API. Can be useful for CORS configuration."
+  value = aws_apigatewayv2_api.api_gateway.api_endpoint
+}
